5df56edde486f1ad0def04b1d84793e29b67ee5b
OPR_A <opr.a.ruby@gmail.com>
2020-04-22 14:04:52 +0400

    Add def keep_data_to_db stub

--- tabl plan-structure/a_plan_structure.rb
+++ tabl plan-structure/a_plan_structure.rb
@@ -16,3 +16,69 @@
 #
 
 require 'sqlite3'
+
+def keep_data_to_db
+
+  file = File.open 'plan_structure.txt', 'r'
+
+  file.each_line do |line|
+
+    puts line
+    
+  end
+
+  file.close
+
+end  
+
+def test
+  @db.execute 'INSERT INTO plan_structuret 
+        (
+            year,
+            name,
+            code_name,
+            level,
+            flag_r,
+            code_color,
+            sp,
+            eiz,
+            code
+        ) 
+        VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ? )', 
+        [
+            data[1],  
+            data[2],
+            data[3],
+            data[4],
+            data[5],
+            data[6],
+            data[7],
+            data[9]
+        ]
+        
+  puts "Данные в таблуцу внесены"
+  
+end
+
+# +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+@db = SQLite3::Database.new 'development.sqlite'
+                          #
+@db.execute 'CREATE TABLE IF NOT EXISTS       
+        "plan_structure" 
+        (
+            "id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+            "year" INTEGER,
+            "name" TEXT,
+            "code_name" TEXT,
+            "level" INTEGER,
+            "flag_r" BOLEAN,
+            "code_color" TEXT,
+            "sp" TEXT,
+            "eiz" TEXT,
+            "code" TEXT
+        )'
+
+keep_data_to_db
+        
+@db.close
